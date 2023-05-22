23f9b7fe5c07de7f7df28f3f1a9bd85dd25a30fc
joao <jpcarvalhof@outlook.com>
2023-05-21 22:16:05 -0300

    junção dos arquivos e integração firebase

--- lib/Emergencia.dart
+++ lib/Emergencia.dart
@@ -1,23 +1,155 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:camera_camera/camera_camera.dart';
+import 'package:camera/camera.dart';
+import 'firebase_options.dart';
 
-class Emergencia extends StatelessWidget {
-  const Emergencia({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "main",
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Página emergência')),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: (){
-              Navigator.pop(context);
-            },
-            child: const Text('Voltar'),
-          ),
-        )
-      ),
-    );
-  }
-}
+ class TelaEmergencia extends StatefulWidget {
+   const TelaEmergencia({
+     super.key,
+   });
+
+   @override
+   TelaEmergenciaState createState() {
+     return TelaEmergenciaState();
+   }
+ }
+
+ class TelaEmergenciaState extends State<TelaEmergencia> {
+
+   XFile? filePhoto;
+
+   // controler para observar o TextFormField
+   final controllerNome = TextEditingController();
+   final controllerNum = TextEditingController();
+
+
+   final FirebaseStorage storage = FirebaseStorage.instance;
+
+   final _formKey = GlobalKey<FormState>();
+
+   // Cria uma referência para a colecao emergencias no firestore.
+   CollectionReference emergencias = FirebaseFirestore.instance.collection('emergencias');
+
+   // funcao que insere no firestore o nome passsado como parametro
+   Future<void> chamarEmergencia(String nome, String numero) {
+     return emergencias
+         .doc(nome).set({
+       'nome': nome,
+       'numero': numero
+     }).then((value) => print("Emergência Enviada!"))
+         .catchError((error) => print("Erro ao adicionar: $error"));
+   }
+
+   Future<void> uploadPhoto(String path, String numero) async{
+     File file = File(path);
+     try{
+       String ref = 'imagenspaciente/foto-${numero}.jpg';
+       await storage.ref(ref).putFile(file);
+     }on FirebaseException catch(e) {
+       throw Exception('Erro no upload: ${e}');
+     }
+
+   }
+
+   Future<XFile?> tirarFoto() async{
+     final ImagePicker _picker = ImagePicker();
+     XFile? image = await _picker.pickImage(source: ImageSource.camera);
+     return image;
+   }
+
+
+   @override
+   Widget build(BuildContext context) {
+     return Material(
+       key: _formKey,
+       child:  Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget> [
+           Padding(
+             padding: const EdgeInsets.all(20.0),
+             child: TextFormField(
+               controller: controllerNome,
+               validator: (value) {
+                 if (value == null || value.isEmpty) {
+                   return 'Digite seu nome';
+                 }
+                 return null;
+               },
+             ),
+           ),
+
+           Padding(
+               padding: const EdgeInsets.all(16.0),
+               child: TextFormField(
+                 controller: controllerNum,
+                 validator:  (value){
+                   if (value == null || value.isEmpty) {
+                     return 'Digite seu Telefone';
+                   }
+                   return null;
+                 },
+               )
+           ),
+
+
+
+           Padding(
+             padding: const EdgeInsets.all(12.0),
+             child:ElevatedButton.icon(
+               onPressed: () async {
+                 try {
+                   filePhoto = await tirarFoto();
+                 } catch (e) {
+                   ScaffoldMessenger.of(context).showSnackBar(
+                     const SnackBar(content: Text('Erro')),
+                   );
+                 }
+               },
+               style: ElevatedButton.styleFrom(
+                   backgroundColor: Colors.teal
+               ),
+               icon: const Icon (Icons.camera_alt_outlined),
+               label: const Padding(
+                   padding: EdgeInsets.all(12.0),
+                   child:Text('TIRAR FOTO')
+               ),
+
+             ),
+           ),
+
+
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: ElevatedButton(
+               onPressed: () {
+                 if (_formKey.currentState!.validate()) {
+                   chamarEmergencia(controllerNome.text, controllerNum.text);
+                   uploadPhoto(filePhoto!.path, controllerNum.text);
+                   ScaffoldMessenger.of(context).showSnackBar(
+                     const SnackBar(content: Text('Gravando dados no Firestore...')),
+                   );
+                 }
+                 else{
+                   ScaffoldMessenger.of(context).showSnackBar(
+                     const SnackBar(content: Text('ERRO: PREENCHA OS DADOS')),
+                   );
+                 }
+               },
+               style: ElevatedButton.styleFrom(
+                   backgroundColor: Colors.red
+               ),
+               child: const Text('ENVIAR EMERGÊNCIA'),
+             ),
+           ),
+
+         ],
+       ),
+     );
+   }
+ }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'dart:typed_data';
+import 'package:meta/meta.dart';
 
+import './base.dart';
 import 'package:socorrista1/main.dart';
 
 void main() {
@@ -27,4 +30,51 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+}
+
+class XFile extends XFileBase {
+  /// Construct a CrossFile object from its path.
+  ///
+  /// Optionally, this can be initialized with `bytes` and `length`
+  /// so no http requests are performed to retrieve data later.
+  ///
+  /// `name` may be passed from the outside, for those cases where the effective
+  /// `path` of the file doesn't match what the user sees when selecting it
+  /// (like in web)
+  XFile(
+      String super.path, {
+        String? mimeType,
+        String? name,
+        int? length,
+        Uint8List? bytes,
+        DateTime? lastModified,
+        @visibleForTesting CrossFileTestOverrides? overrides,
+      }) {
+    throw UnimplementedError(
+        'CrossFile is not available in your current platform.');
+  }
+
+  /// Construct a CrossFile object from its data
+  XFile.fromData(
+      Uint8List bytes, {
+        String? mimeType,
+        String? name,
+        int? length,
+        DateTime? lastModified,
+        String? path,
+        @visibleForTesting CrossFileTestOverrides? overrides,
+      }) : super(path) {
+    throw UnimplementedError(
+        'CrossFile is not available in your current platform.');
+  }
+}
+
+/// Overrides some functions of CrossFile for testing purposes
+@visibleForTesting
+class CrossFileTestOverrides {
+  /// Default constructor for overrides
+  CrossFileTestOverrides({required this.createAnchorElement});
+
+  /// For overriding the creation of the file input element.
+  dynamic Function(String href, String suggestedName) createAnchorElement;
 }
